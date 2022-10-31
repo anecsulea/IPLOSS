794b2f96f0128051a71f0e61606f301fb7152d19
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-10-31 23:39:43 +0100

    changed path for pbil

--- scripts/transcript_assembly/combine.annotations.sh
+++ scripts/transcript_assembly/combine.annotations.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 export sp=$1
-export ref=$2
-export sample=$3
-export cluster=$4
+export cluster=$2
 
 #############################################################################
 
@@ -11,8 +9,12 @@ if [ ${cluster} = "cloud" ]; then
     export path=/home/ubuntu/data/mydatalocal/IPLOSS
 fi
 
+if [ ${cluster} = "pbil" ]||[ ${cluster} = "pbillocal" ]; then
+    export path=/beegfs/data/necsulea/IPLOSS
+fi
+
 export pathEnsembl=${path}/data/ensembl_annotations/${sp}
-export pathStringTieAssembly=${path}/results/stringtie_assembly/${sp}/reference_${ref}_${sample}
+export pathStringTieAssembly=${path}/results/stringtie_assembly/${sp}
 export pathScripts=${path}/scripts/transcript_assembly
 
 #############################################################################

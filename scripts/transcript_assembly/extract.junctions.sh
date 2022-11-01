af6887c70942d8ecfdf36c723c4eaedc55d2ffe7
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-11-01 14:36:04 +0100

    changed path

--- scripts/transcript_assembly/extract.junctions.sh
+++ scripts/transcript_assembly/extract.junctions.sh
@@ -17,7 +17,7 @@ fi
 export ensrelease=103
 
 export pathAlignments=${path}/results/RNASeq_alignments/${sp}/${sample}
-export pathSequence=${path}/data/genome_indexes/${sp}/genome_ensembl${ensrelease}.fa
+export pathSequence=${path}/results/genome_indexes/${sp}/genome_ensembl${ensrelease}.fa
 export pathScripts=${path}/scripts/transcript_assembly
 
 ######################################################################################

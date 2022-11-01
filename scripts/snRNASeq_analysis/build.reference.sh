741f591ae966f7d14d2764e28dfe71e94dcbcde4
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-11-01 13:15:55 +0100

    started uploading snRNA scripts

---
+++ scripts/snRNASeq_analysis/build.reference.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+## adapted from script by Menghan Wang
+
+export species=$1
+export annot=$2
+export cluster=$3
+
+####################################################################################
+
+if [ ${cluster} = "pbil" ]||[ ${cluster} = "pbillocal" ]; then
+    export path=/beegfs/data/necsulea/IPLOSS
+    export pathTools=/beegfs/home/necsulea/Tools
+fi
+
+if [ ${cluster} = "cloud" ]; then
+    export path=/ifb/data/mydatalocal/IPLOSS
+    export pathTools=/ifb/data/mydatalocal/Tools
+fi
+
+####################################################################################
+
+export pathGenome=${path}/data/genome_sequences/${sp}
+export pathEnsembl=${path}/data/ensembl_annotations/${sp}
+export pathStringTie=${path}/results/stringtie_assembly/${sp}
+export pathIndexes=${path}/results/snRNA_indexes/${sp}
+
+export release=103
+
+## cell ranger version 7.0.1
+
+####################################################################################
+
+if [ -e ${pathIndexes} ]; then
+    echo "path output already there"
+else
+    mkdir -p ${pathIndexes} 
+fi
+
+####################################################################################
+
+if [ ${annot} = "Ensembl" ]; then
+    export pathGTF=${pathEnsembl}/AllTranscripts_Ensembl${release}.gtf
+fi
+
+####################################################################################
+
+if [ ${annot} = "EnsemblStringTie" ]; then
+    export pathGTF=${pathStringTie}/combined_annotations_StringTie_Ensembl.gtf
+fi
+
+####################################################################################
+
+cellranger mkref --genome=${pathIndexes}/${annot}  --fasta=${pathGenome}/genome_ensembl${release}.fa --genes=${pathGTF}
+
+########################################################################

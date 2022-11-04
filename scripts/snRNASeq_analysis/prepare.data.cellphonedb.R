5b985e794266d702bf4a2a3b06487707351f6710
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-11-04 11:16:45 +0100

    final data for cell phone db

---
+++ scripts/snRNASeq_analysis/prepare.data.cellphonedb.R
@@ -0,0 +1,40 @@
+#####################################################################
+
+library(Seurat)
+library(tximport)
+
+#####################################################################
+
+pathResults="../../results/snRNASeq_analysis/"
+annot="EnsemblStringTie"
+
+#####################################################################
+
+samples <- list()
+samples[["Chicken"]] <- "Gg_W_GT_male_33_scRNA"
+samples[["Duck"]] <- c("Ap_W_GT_male_33_scRNA", "Ap_W_GT_female_33_scRNA")
+
+#####################################################################
+
+for(sp in names(samples)){
+    for(sample in samples[[sp]]){
+
+        load(paste(pathResults, sp, "/",annot, "/",sample, "/seurat.object.after.clustering.RData",sep=""))
+
+        so <- filtered.seurat
+
+        ## prepare data for CellPhoneDB
+        count_raw <- GetAssayData(object=so, slot="counts")
+        count_raw <- as.matrix(count_raw)
+
+        count_norm <- apply(count_raw, 2, function(x) (x/sum(x))*10000)
+        write.table(count_norm, paste('data_for_cellphonedb/cellphonedb_count_',sp,'_',sample,'_',annot,'.txt',sep=""), sep='\t', quote=F)
+
+        ## generating meta file
+        all_meta <- so[[]]
+        meta_data <- cbind(rownames(all_meta), all_meta[,c("seurat_clusters")])
+        write.table(meta_data,  paste('data_for_cellphonedb/cellphonedb_meta_',sp,'_',sample,'_', annot, '.txt',sep=""), sep='\t', quote=F, row.names=F)
+    }
+}
+
+#####################################################################

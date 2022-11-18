f10d22116609d9bc9e723959035a8552641dce3d
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-11-18 13:58:22 +0100

    changed output folder

--- scripts/snRNASeq_analysis/prepare.data.cellphonedb.R
+++ scripts/snRNASeq_analysis/prepare.data.cellphonedb.R
@@ -1,7 +1,6 @@
 #####################################################################
 
 library(Seurat)
-library(tximport)
 
 #####################################################################
 
@@ -28,12 +27,12 @@ for(sp in names(samples)){
         count_raw <- as.matrix(count_raw)
 
         count_norm <- apply(count_raw, 2, function(x) (x/sum(x))*10000)
-        write.table(count_norm, paste('data_for_cellphonedb/cellphonedb_count_',sp,'_',sample,'_',annot,'.txt',sep=""), sep='\t', quote=F)
+        write.table(count_norm, paste(pathResults, "data_for_cellphonedb/cellphonedb_count_",sp,"_",sample,"_",annot,".txt",sep=""), sep="\t", quote=F)
 
         ## generating meta file
         all_meta <- so[[]]
         meta_data <- cbind(rownames(all_meta), all_meta[,c("seurat_clusters")])
-        write.table(meta_data,  paste('data_for_cellphonedb/cellphonedb_meta_',sp,'_',sample,'_', annot, '.txt',sep=""), sep='\t', quote=F, row.names=F)
+        write.table(meta_data,  paste(pathResults, "data_for_cellphonedb/cellphonedb_meta_",sp,"_",sample,"_", annot, ".txt",sep=""), sep="\t", quote=F, row.names=F)
     }
 }
 

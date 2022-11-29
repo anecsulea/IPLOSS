2199ec31dd898aab4917e9dd83a7674e39f1873b
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-11-29 19:14:06 +0100

    check if already done

--- scripts/detect_conserved_elements/compute.average.scores.sh
+++ scripts/detect_conserved_elements/compute.average.scores.sh
@@ -17,10 +17,14 @@ export pathScripts=${path}/scripts/detect_conserved_elements
 
 for chr in {1..29} Z W
 do
-    if [ -e ${pathResults}/most_conserved_${chr}.bed ]; then
-	perl ${pathScripts}/compute.average.scores.pl --chr=${chr} --pathScores=${pathResults}/phastcons_scores_${chr}.wig --pathCoords=${pathResults}/most_conserved_${chr}.bed --pathOutput=${pathResults}/most_conserved_${chr}.txt
+    if [ -e ${pathResults}/most_conserved_${chr}.txt ]; then
+	echo "already done"
     else
-	echo "cannot find conserved elements for "${chr}
+	if [ -e ${pathResults}/most_conserved_${chr}.bed ]; then
+	    perl ${pathScripts}/compute.average.scores.pl --chr=${chr} --pathScores=${pathResults}/phastcons_scores_${chr}.wig --pathCoords=${pathResults}/most_conserved_${chr}.bed --pathOutput=${pathResults}/most_conserved_${chr}.txt
+	else
+	    echo "cannot find conserved elements for "${chr}
+	fi
     fi
 done
 

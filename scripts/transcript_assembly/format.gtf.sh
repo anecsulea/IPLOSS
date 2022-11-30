9d8c6fb86ca04a9b1f366528a5c58159f5aa6914
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-11-30 16:41:49 +0100

    add CDS info

--- scripts/transcript_assembly/format.gtf.sh
+++ scripts/transcript_assembly/format.gtf.sh
@@ -18,8 +18,20 @@ export pathEnsembl=${path}/data/ensembl_annotations/${sp}
 export pathStringTieAssembly=${path}/results/stringtie_assembly/${sp}
 export pathScripts=${path}/scripts/transcript_assembly
 
+export release=103
+
 #############################################################################
 
-perl ${pathScripts}/format.gtf.pl --pathInputGTF=${pathStringTieAssembly}/combined_annotations_StringTie_Ensembl.gtf --pathOutputGTF=${pathStringTieAssembly}/combined_annotations_StringTie_Ensembl_withTxInfo.gtf
+if [ -e ${pathStringTieAssembly}/combined_annotations_StringTie_Ensembl_withTxInfo.gtf ]; then
+    echo "file with tx info already there"
+else
+    perl ${pathScripts}/format.gtf.pl --pathInputGTF=${pathStringTieAssembly}/combined_annotations_StringTie_Ensembl.gtf --pathOutputGTF=${pathStringTieAssembly}/combined_annotations_StringTie_Ensembl_withTxInfo.gtf
+fi
+
+#############################################################################
+
+cp ${pathStringTieAssembly}/combined_annotations_StringTie_Ensembl_withTxInfo.gtf ${pathStringTieAssembly}/combined_annotations_StringTie_Ensembl_withTxInfo_withCDSInfo.gtf
+
+grep $'\t'CDS$'\t' ${pathEnsembl}/AllTranscripts_Ensembl103.gtf >> ${pathStringTieAssembly}/combined_annotations_StringTie_Ensembl_withTxInfo_withCDSInfo.gtf
 
 #############################################################################

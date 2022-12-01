8b96f642fc2d28bb4ab713fb7a9b8e9b69f7fb2a
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-12-01 14:25:12 +0100

    updated annotation

--- scripts/snATACSeq_analysis/prepare.BSannot.R
+++ scripts/snATACSeq_analysis/prepare.BSannot.R
@@ -1,43 +1,84 @@
+#################################################################################
 
-##########################
-#Creating a Custom ArchRGenome
-##########################
+## adapted from original script by Menghan Wang
+## https://www.archrproject.com/bookdown/getting-set-up.html
 
-#https://www.archrproject.com/bookdown/getting-set-up.html
-library(BSgenome.Ggallus.ensembl.galGal6) #library(BSgenome.Ggallus.UCSC.galGal6)
-# in order to keep chrZ and chrW, first change filter=TRUE to filter=FALSE
-genomeAnnotation = createGenomeAnnotation(genome = "BSgenome.Ggallus.ensembl.galGal6",filter=FALSE)
-# then manually retain autosomes and sex chromosomes
-genomeAnnotation$chromSizes=genomeAnnotation$chromSizes[2:35]
-unique(seqnames(genomeAnnotation$chromSizes));seqlengths(BSgenome.Ggallus.ensembl.galGal6)
+#################################################################################
 
-##gtf need to have gene_name
-#awk -F $'\t' '$9 ~ /gene_name/ {print $0}' Gg6_extended_060420.gtf > Gg6_extended_060420.filter.gtf
+library(ArchR)
+library(BSgenome)
+library(GenomeInfoDb)
+library(ensembldb)
 
-anno_path <- "/scicore/home/tschoppp/GROUP/references/genomes/forGenitalia/"
-db <- ensDbFromGtf(gtf=paste0(anno_path,"Gg6_extended_060420.filter.gtf"),
-                   organism = "Gallus_gallus", genomeVersion = "GRCg6a", version = 97)
-edb <- EnsDb(db); rm(db)
-edb = addFilter(edb, filter = GeneBiotypeFilter('protein_coding',"=="))
-# change the settings here as well, to keep consistent
-edb = addFilter(edb, filter = SeqNameFilter(c('AADN05001525.1','KZ626819.1','KZ626826.1','KZ626830.1','MT',
-                                              'KZ626833.1','KZ626834.1','KZ626835.1','KZ626836.1','KZ626839.1'),"!=")) #'Z','W',
-gene.ranges <- genes(edb); length(gene.ranges$gene_id)
-tss.ranges <- GRanges(seqnames = seqnames(gene.ranges),
-                      ranges = IRanges(start = start(gene.ranges), width = 2),
-                      strand = strand(gene.ranges)
-)
-exon.ranges = exons(edb)
-geneAnnotation = createGeneAnnotation(
-  TSS = tss.ranges,
-  exons = exon.ranges,
-  genes = gene.ranges
-)
-unique(seqnames(geneAnnotation$genes))
+#################################################################################
 
-save_path = "/scicore/home/tschoppp/wang0007/genitalia/"
-saveRDS(genomeAnnotation, paste0(save_path,"genomeAnnotation_withZW.rds"))
-saveRDS(geneAnnotation, paste0(save_path,"geneAnnotation_withZW.rds"))
+pathEnsembl="../../data/ensembl_annotations/"
+pathEnsemblStringTie="../../results/stringtie_assembly/"
+pathResults="../../results/snATACSeq_analysis/"
 
+#################################################################################
 
+for(sp in c("Chicken", "Duck")){
 
+    if(sp=="Chicken"){
+        library(BSgenome.Ggallus)
+
+        genomeAnnotation = createGenomeAnnotation(genome = "BSgenome.Ggallus",filter=FALSE)
+
+        ## we remove mitochondria, W chromosome, unassembled contigs
+        genomeAnnotation$chromSizes=genomeAnnotation$chromSizes[c(1:32, 34)]
+
+        organism="Gallus_gallus"
+    }
+
+    if(sp=="Duck"){
+        library(BSgenome.Aplatyrhynchos)
+
+        genomeAnnotation = createGenomeAnnotation(genome = "BSgenome.Aplatyrhynchos",filter=FALSE)
+
+        organism="Anas_platyrhynchos_platyrhynchos"
+    }
+
+    chrlist = as.character(unique(seqnames(genomeAnnotation$chromSizes)))
+
+    path.annot = paste(pathEnsembl, sp, "/AllTranscripts_Ensembl103.gtf",sep="")
+
+    if(sp=="Chicken"){
+        edb = ensDbFromGtf(gtf=path.annot, organism = organism, genomeVersion = "GRCg6a", version = 103)
+    }
+
+    if(sp=="Duck"){
+        edb = ensDbFromGtf(gtf=path.annot, organism = organism, genomeVersion = "CAU_duck1.0", version = 103)
+    }
+
+    edb = EnsDb(edb)
+
+    ## keep protein-coding genes
+    edb = addFilter(edb, filter = GeneBiotypeFilter('protein_coding',"=="))
+
+    ## same chromosomes as above
+    edb = addFilter(edb, filter = SeqNameFilter(chrlist,"=="))
+
+    gene.ranges = genes(edb)
+
+    gene.strand=strand(gene.ranges)
+    gene.start=start(gene.ranges)
+    gene.end=end(gene.ranges)
+
+    tss.pos=rep(NA, length(gene.start))
+    tss.pos[which(gene.strand=="+")]=gene.start[which(gene.strand=="+")]
+    tss.pos[which(gene.strand=="-")]=gene.end[which(gene.strand=="-")]
+
+    tss.ranges = GRanges(seqnames = seqnames(gene.ranges), ranges = IRanges(start = tss.pos, width = 2), strand = strand(gene.ranges))
+
+    exon.ranges = exons(edb)
+
+    geneAnnotation = createGeneAnnotation(TSS = tss.ranges, exons = exon.ranges, genes = gene.ranges)
+
+    print(unique(seqnames(geneAnnotation$genes)))
+
+    save(genomeAnnotation, file=paste(pathResults, sp, "/Ensembl/genomeAnnotation.RData",sep=""))
+    save(geneAnnotation, file=paste(pathResults, sp, "/Ensembl/geneAnnotation.RData",sep=""))
+}
+
+#################################################################################
